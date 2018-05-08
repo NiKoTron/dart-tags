8e2abf1446efb7bdf6a0c950ca2b23b80ee1c2bd
nsimonov <nikolay.simonov@harman.com>
2018-05-08 15:04:24 +0300

    add APIC reading;

--- lib/src/readers/id3v2.dart
+++ lib/src/readers/id3v2.dart
@@ -4,86 +4,84 @@ import 'dart:convert';
 import 'package:dart_tags/src/readers/reader.dart';
 
 class ID3V2Reader extends Reader {
-
-  // v2.3+ frames  
+  // v2.3+ frames
   final _frames = {
-    "AENC" : "Audio encryption",
-    "APIC" : "Attached picture",
-    "COMM" : "Comments",
-    "COMR" : "Commercial frame",
-    "ENCR" : "Encryption method registration",
-    "EQUA" : "Equalization",
-    "ETCO" : "Event timing codes",
-    "GEOB" : "General encapsulated object",
-    "GRID" : "Group identification registration",
-    "IPLS" : "Involved people list",
-    "LINK" : "Linked information",
-    "MCDI" : "Music CD identifier",
-    "MLLT" : "MPEG location lookup table",
-    "OWNE" : "Ownership frame",
-    "PRIV" : "Private frame",
-    "PCNT" : "Play counter",
-    "POPM" : "Popularimeter",
-    "POSS" : "Position synchronisation frame",
-    "RBUF" : "Recommended buffer size",
-    "RVAD" : "Relative volume adjustment",
-    "RVRB" : "Reverb",
-    "SYLT" : "Synchronized lyric/text",
-    "SYTC" : "Synchronized tempo codes",
-    "TALB" : "Album/Movie/Show title",
-    "TBPM" : "BPM (beats per minute)",
-    "TCOM" : "Composer",
-    "TCON" : "Content type",
-    "TCOP" : "Copyright message",
-    "TDAT" : "Date",
-    "TDLY" : "Playlist delay",
-    "TENC" : "Encoded by",
-    "TEXT" : "Lyricist/Text writer",
-    "TFLT" : "File type",
-    "TIME" : "Time",
-    "TIT1" : "Content group description",
-    "TIT2" : "Title/songname/content description",
-    "TIT3" : "Subtitle/Description refinement",
-    "TKEY" : "Initial key",
-    "TLAN" : "Language(s)",
-    "TLEN" : "Length",
-    "TMED" : "Media type",
-    "TOAL" : "Original album/movie/show title",
-    "TOFN" : "Original filename",
-    "TOLY" : "Original lyricist(s)/text writer(s)",
-    "TOPE" : "Original artist(s)/performer(s)",
-    "TORY" : "Original release year",
-    "TOWN" : "File owner/licensee",
-    "TPE1" : "Lead performer(s)/Soloist(s)",
-    "TPE2" : "Band/orchestra/accompaniment",
-    "TPE3" : "Conductor/performer refinement",
-    "TPE4" : "Interpreted, remixed, or otherwise modified by",
-    "TPOS" : "Part of a set",
-    "TPUB" : "Publisher",
-    "TRCK" : "Track number/Position in set",
-    "TRDA" : "Recording dates",
-    "TRSN" : "Internet radio station name",
-    "TRSO" : "Internet radio station owner",
-    "TSIZ" : "Size",
-    "TSRC" : "ISRC (international standard recording code)",
-    "TSSE" : "Software/Hardware and settings used for encoding",
-    "TYER" : "Year",
-    "TXXX" : "User defined text information frame",
-    "UFID" : "Unique file identifier",
-    "USER" : "Terms of use",
-    "USLT" : "Unsychronized lyric/text transcription",
-    "WCOM" : "Commercial information",
-    "WCOP" : "Copyright/Legal information",
-    "WOAF" : "Official audio file webpage",
-    "WOAR" : "Official artist/performer webpage",
-    "WOAS" : "Official audio source webpage",
-    "WORS" : "Official internet radio station homepage",
-    "WPAY" : "Payment",
-    "WPUB" : "Publishers official webpage",
-    "WXXX" : "User defined URL link frame",
-};
-  
-  
+    "AENC": "Audio encryption",
+    "APIC": "Attached picture",
+    "COMM": "Comments",
+    "COMR": "Commercial frame",
+    "ENCR": "Encryption method registration",
+    "EQUA": "Equalization",
+    "ETCO": "Event timing codes",
+    "GEOB": "General encapsulated object",
+    "GRID": "Group identification registration",
+    "IPLS": "Involved people list",
+    "LINK": "Linked information",
+    "MCDI": "Music CD identifier",
+    "MLLT": "MPEG location lookup table",
+    "OWNE": "Ownership frame",
+    "PRIV": "Private frame",
+    "PCNT": "Play counter",
+    "POPM": "Popularimeter",
+    "POSS": "Position synchronisation frame",
+    "RBUF": "Recommended buffer size",
+    "RVAD": "Relative volume adjustment",
+    "RVRB": "Reverb",
+    "SYLT": "Synchronized lyric/text",
+    "SYTC": "Synchronized tempo codes",
+    "TALB": "Album/Movie/Show title",
+    "TBPM": "BPM (beats per minute)",
+    "TCOM": "Composer",
+    "TCON": "Content type",
+    "TCOP": "Copyright message",
+    "TDAT": "Date",
+    "TDLY": "Playlist delay",
+    "TENC": "Encoded by",
+    "TEXT": "Lyricist/Text writer",
+    "TFLT": "File type",
+    "TIME": "Time",
+    "TIT1": "Content group description",
+    "TIT2": "Title/songname/content description",
+    "TIT3": "Subtitle/Description refinement",
+    "TKEY": "Initial key",
+    "TLAN": "Language(s)",
+    "TLEN": "Length",
+    "TMED": "Media type",
+    "TOAL": "Original album/movie/show title",
+    "TOFN": "Original filename",
+    "TOLY": "Original lyricist(s)/text writer(s)",
+    "TOPE": "Original artist(s)/performer(s)",
+    "TORY": "Original release year",
+    "TOWN": "File owner/licensee",
+    "TPE1": "Lead performer(s)/Soloist(s)",
+    "TPE2": "Band/orchestra/accompaniment",
+    "TPE3": "Conductor/performer refinement",
+    "TPE4": "Interpreted, remixed, or otherwise modified by",
+    "TPOS": "Part of a set",
+    "TPUB": "Publisher",
+    "TRCK": "Track number/Position in set",
+    "TRDA": "Recording dates",
+    "TRSN": "Internet radio station name",
+    "TRSO": "Internet radio station owner",
+    "TSIZ": "Size",
+    "TSRC": "ISRC (international standard recording code)",
+    "TSSE": "Software/Hardware and settings used for encoding",
+    "TYER": "Year",
+    "TXXX": "User defined text information frame",
+    "UFID": "Unique file identifier",
+    "USER": "Terms of use",
+    "USLT": "Unsychronized lyric/text transcription",
+    "WCOM": "Commercial information",
+    "WCOP": "Copyright/Legal information",
+    "WOAF": "Official audio file webpage",
+    "WOAR": "Official artist/performer webpage",
+    "WOAS": "Official audio source webpage",
+    "WORS": "Official internet radio station homepage",
+    "WPAY": "Payment",
+    "WPUB": "Publishers official webpage",
+    "WXXX": "User defined URL link frame",
+  };
+
   final _frameShortcutsID3V2_3 = {
     "TIT2": "title",
     "TPE1": "artist",
@@ -96,7 +94,7 @@ class ID3V2Reader extends Reader {
     "USLT": "lyrics",
   };
 
-//todo support v2.2
+  //todo support v2.2
   final _frameShortcutsID3V2_2 = {
     "TT2": "title",
     "TP1": "artist",
@@ -110,7 +108,7 @@ class ID3V2Reader extends Reader {
   };
 
   final _frameLegacy = {
-      "BUF": "RBUF",
+    "BUF": "RBUF",
     "COM": "COMM",
     "CRA": "AENC",
     "EQU": "EQUA",
@@ -171,6 +169,39 @@ class ID3V2Reader extends Reader {
     "WXX": "WXXX",
   };
 
+
+  final _picturesType = [
+		"Other",
+		"32x32 pixels 'file icon' (PNG only)",
+		"Other file icon",
+		"Cover (front)",
+		"Cover (back)",
+		"Leaflet page",
+		"Media (e.g. lable side of CD)",
+		"Lead artist/lead performer/soloist",
+		"Artist/performer",
+		"Conductor",
+		"Band/Orchestra",
+		"Composer",
+		"Lyricist/text writer",
+		"Recording Location",
+		"During recording",
+		"During performance",
+		"Movie/video screen capture",
+		"A bright coloured fish",
+		"Illustration",
+		"Band/artist logotype",
+		"Publisher/Studio logotype",
+  ];
+
+  static const ISO_8859_1 = 0x00; // [ISO-8859-1]. Terminated with $00.
+  static const UTF_16 =
+      0x01; // [UTF-16] encoded Unicode [UNICODE] with BOM. All strings in the same frame SHALL have the same byteorder. Terminated with $00 00.
+  static const UTF_16BE =
+      0x02; // [UTF-16] encoded Unicode [UNICODE] without BOM. Terminated with $00 00.
+  static const UTF8 =
+      0x03; // [UTF-8] encoded Unicode [UNICODE]. Terminated with $00.
+
   int version_o1 = 2;
   int version_o2;
   int version_o3;
@@ -193,10 +224,7 @@ class ID3V2Reader extends Reader {
 
     var flag = sBytes[5];
 
-    var size = sBytes[6] << 0x15;
-    size += sBytes[7] << 14;
-    size += sBytes[8] << 7;
-    size += sBytes[9];
+    var size = _sizeOf(sBytes.sublist(6, 10));
 
     var offset = 10;
 
@@ -204,23 +232,47 @@ class ID3V2Reader extends Reader {
 
     while (end) {
       var tag = utf8.decode(sBytes.sublist(offset, offset + 4));
-      var len = sBytes[offset + 7];
-      
-      print(tag);
+      var encoding = _getEncoding(sBytes[offset + HEADER_LEN]);
+
+      var len = _sizeOf(sBytes.sublist(offset + 4, offset + 8));
+
       if (len > 0 && _isValidTag(tag)) {
-        if (tag == 'APIC') {
-          tags[tag] = "picture";
-        }
-        else {
-          tags[_getTag(tag)] = utf8.decode(sBytes.sublist(
-            offset + HEADER_LEN + 1, offset + HEADER_LEN + len - 1));
+        switch (tag) {
+          case 'APIC':
+            tags[tag] = _parsePicture(sBytes.sublist(offset + HEADER_LEN + 1, offset + HEADER_LEN + len), encoding);
+            break;
+          default:
+            tags[_getTag(tag)] = encoding.decode(sBytes.sublist(offset + HEADER_LEN + 1, offset + HEADER_LEN + len - 1));
         }
       }
+
       offset = offset + HEADER_LEN + len;
       end = offset < size;
     }
 
     return tags;
+  }
+
+  int _sizeOf(List<int> block) {
+    assert(block.length == 4);
+
+    var len = block[0] << 21;
+    len += block[1] << 14;
+    len += block[2] << 7;
+    len += block[3];
+
+    return len;
+  }
+
+  Encoding _getEncoding(int type) {
+    switch (type) {
+      case ISO_8859_1:
+        return latin1;
+      case UTF8:
+        return utf8;
+      default:
+        return new UTF16();
+    }
   }
 
   String _getTag(String tag) {
@@ -229,7 +281,71 @@ class ID3V2Reader extends Reader {
         : tag;
   }
 
-  bool _isValidTag(String tag){
+  bool _isValidTag(String tag) {
     return _frames.containsKey(tag);
+  }
+
+
+  final _fields = [
+    "mime",
+    "image_type",
+    "description",
+    "data"
+  ];
+
+  _parsePicture(List<int> sublist, Encoding enc) {
+    var itr = sublist.iterator;
+    var buff = [];
+    
+    var dt = {};
+
+    var cont = 0;
+  
+    while (itr.moveNext() && cont < 4) {
+      final crnt = itr.current;
+      if (crnt == 0x00 && cont < 3) {
+        if(cont == 1) //get first byte to img type, other to description;
+        {
+          dt[_fields[cont]] = _picturesType[buff[0]];
+          cont++;
+          dt[_fields[cont]] = enc.decode(buff.sublist(1));
+        } else {
+          dt[_fields[cont]] = enc.decode(buff);
+        }
+        buff = [];
+        cont++;
+        continue;
+      }
+      buff.add(crnt);
+    }
+
+    dt[_fields[cont]] = buff;
+
+    return dt;
+  }
+}
+
+class UTF16 extends Encoding {
+  @override
+  Converter<List<int>, String> get decoder => new _UTF16Decoder();
+
+  @override
+  Converter<String, List<int>> get encoder => new _UTF16Enoder();
+
+  @override
+  String get name => 'utf16';
+}
+
+class _UTF16Decoder extends Converter<List<int>, String> {
+  @override
+  String convert(List<int> input) {
+    String.fromCharCodes(input);
+  }
+}
+
+class _UTF16Enoder extends Converter<String, List<int>> {
+  @override
+  List<int> convert(String input) {
+    return input.runes.toList();
   }
 }
