6921a411d21a3d51dea24fa07369bb722b8cb3ba
NiKoTron <nikotron@rocketmail.com>
2019-03-19 16:45:15 +0300

    move constants to separate file, some cleanup

--- lib/src/writers/id3v2.dart
+++ lib/src/writers/id3v2.dart
@@ -8,18 +8,17 @@ class ID3V2Writer extends Writer {
   ID3V2Writer() : super('ID3', '2.4');
 
   @override
-  Future<List<int>> prepareTag(Tag tag) {}
+  Future<List<int>> prepareTag(Tag tag) {
+    
+  }
 
   @override
   Future<List<int>> removeExistingTag(List<int> bytes) {
     final c = new Completer<List<int>>.sync();
-    if (new Utf8Codec(allowMalformed: true).decode(bytes.sublist(0, 3)) !=
-        'ID3') {
-      c.complete(bytes);
-    } else {
-      final size = _sizeOf(bytes.sublist(6, 10));
-      c.complete(bytes.sublist(size));
-    }
+
+    Utf8Codec(allowMalformed: true).decode(bytes.sublist(0, 3)) != 'ID3'
+        ? c.complete(bytes)
+        : c.complete(bytes.sublist(_sizeOf(bytes.sublist(6, 10))));
 
     return c.future;
   }
